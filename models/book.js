9471dd98a4d2992f4d7df75e9f711aa87b20f901
AbreuGeorgie <abreu.georgie@gmail.com>
2023-06-06 17:17:12 +0200

    models book

---
+++ models/book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true }, // - identifiant MongoDB unique de l'utilisateur qui a créé le livre
+  title: { type: String, required: true }, // - titre du livre
+  author: { type: String, required: true }, // - auteur du livre
+  year: { type: Number, required: true }, // - année de publication du livre
+  genre: { type: String, required: true }, // - genre du livre
+  ratings: [
+    {
+      userId: String, // - identifiant MongoDB unique de l'utilisateur qui a noté le livre
+      grade: Number, // - note donnée à un livre
+    },
+  ], // - notes données à un livre
+  averageRating: Number, // - note moyenne du livre
+  imageUrl: { type: String, required: true }, // - image de couverture du livre
+});
+
+module.exports = mongoose.model("Book", bookSchema);
