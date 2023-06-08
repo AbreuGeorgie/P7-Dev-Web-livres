5c000afb0130ff8cd37416ba269972450d6935fa
AbreuGeorgie <abreu.georgie@gmail.com>
2023-06-09 00:08:32 +0200

    formattage

--- models/book.js
+++ models/book.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const bookSchema = mongoose.Schema({
   userId: { type: String, required: true }, // - identifiant MongoDB unique de l'utilisateur qui a créé le livre
