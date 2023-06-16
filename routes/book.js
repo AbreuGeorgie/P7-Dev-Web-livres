f778b0c261bb543f3142d3ef43fdfdcd4cb6efae
AbreuGeorgie <abreu.georgie@gmail.com>
2023-06-16 12:33:14 +0200

    réorganisation de l'ordre des différentes routes

--- routes/book.js
+++ routes/book.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require ('../middleware/auth');
-const multer = require('../middleware/multer-config')
-const resizeImage = require('../middleware/resizeImage')
+const multer = require('../middleware/multer-config');
+const resizeImage = require('../middleware/resizeImage');
 
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
-router.post('/', auth, multer, resizeImage, bookCtrl.createBook);
+router.get('/bestrating', bookCtrl.bestRating);
 router.get('/:id', bookCtrl.getOneBook);
+router.post('/', auth, multer, resizeImage, bookCtrl.createBook);
 router.put('/:id', auth, multer, resizeImage, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.ratingBook);
-router.get('/bestrating', bookCtrl.test);
-module.exports = router;
+
+
+module.exports = router; 
