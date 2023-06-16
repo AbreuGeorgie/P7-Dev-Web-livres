ba91a56ff693cc5aeef0d2c4ab5a8d772a40627f
AbreuGeorgie <abreu.georgie@gmail.com>
2023-06-16 12:28:36 +0200

    ajout des commentaires

--- controllers/book.js
+++ controllers/book.js
@@ -1,168 +1,164 @@
+//GESTION DES OPERATIONS CRUD (CREATE, READ, UPDATE, DELETE)
+
 const Book = require("../models/book");
 const fs = require("fs");
 
-// ajout d'un nouveau livre
+// AJOUT D'UN NOUVEAU LIVRE
 exports.createBook = (req, res, next) => {
-  // stockage de la requête sous forme de JSON
-  const bookObject = JSON.parse(req.body.book);
-  // Suppression du faux _id envoyé par le front
-  delete bookObject._id;
-  // Suppression de _userId
-  delete bookObject._userId;
-  // Création d'une instance de Book
-  const book = new Book({
+
+  const bookObject = JSON.parse(req.body.book); // stockage de la requête sous forme de JSON
+  delete bookObject._id; // Suppression du faux _id envoyé par le front
+  delete bookObject._userId; // Suppression de _userId
+
+  const book = new Book({ // Création d'une instance de Book
     ...bookObject,
-    userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
-  });
-  // sauvegarde dans la base de données
+    userId: req.auth.userId, //id de l'utilisateur authentifié
+    imageUrl: `${req.protocol}://${req.get("host")}/${req.file.path}`}); //url du livre
+  
   book
-    .save()
-    .then(() => res.status(201).json({ book: book }))
-    .catch((error) => res.status(400).json({ error }));
+    .save()// sauvegarde du livre dans la base de données
+    .then(() => res.status(201).json({ book: book }))//réponse json avec le livre créé
+    .catch((error) => res.status(400).json({ error }));//erreur si echec
 };
 
-// modification d'un livre existant
+// MODIFICATION D'UN LIVRE EXISTANT
 exports.modifyBook = (req, res, next) => {
-  // Stockage de la requête en JSON dans une constante
-  const bookObject = req.file
-    ? {
-        ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
+
+  const bookObject = req.file //si image téléchargée avec la requête req.file
+    ? {//si oui mise à jour de bookObject en ajoutant l'url de l'image mise à jour
+        ...JSON.parse(req.body.book),//crée une copie de l'objet à partir de la chaine JSON
+       imageUrl: `${req.protocol}://${req.get("host")}/${req.file.path}`//nouvelle propriété imageUrl contenant l'URL de l'image mise à jour
       }
-    : { ...req.body };
-  // Suppression de _userId auquel on ne peut faire confiance
-  delete bookObject._userId;
-  Book.findOne({ _id: req.params.id })
-    .then((book) => {
-      if (book.userId != req.auth.userId) {
-        res.status(403).json({ message: "Requête non autorisée" });
-      } else {
-        Book.updateOne(
+    : { ...req.body };//si pas d'image téléchargée, alors bookObject est simplement une copie de req.body qui contient les données du livre
+
+  delete bookObject._userId; // Suppression de _userId auquel on ne peut faire confiance
+  
+  Book.findOne({ _id: req.params.id })// recherche du livre correspondant à l'ID fourni dans la requête req.params.id
+    .then((book) => { //si livre trouvé
+      if (book.userId != req.auth.userId) { //verifie si ID de l'utilisateur du livre ne correspond pas à l'id de l'utilisateur authentifié
+        res.status(403).json({ message: "Requête non autorisée" });//ne correspond pas
+      } else { // correspond 
+        Book.updateOne(// met à jour avec les nouvelles données
           { _id: req.params.id },
-          { ...bookObject, _id: req.params.id }
+          { ...bookObject, _id: req.params.id }//créer un nouvel objet en copiant toutes les propriétés de bookObject qui contient les nouvelles données du livre et en ajoutant une propriété _id avec la valeur de req.params.id pour garantir que l'id du livre reste inchangé
         )
-          .then(() => res.status(200).json({ message: "Livre modifié!" }))
-          .catch((error) => res.status(401).json({ error }));
+          .then(() => res.status(200).json({ message: "Livre modifié!" })) //succès
+          .catch((error) => res.status(401).json({ error })); //erreur
       }
     })
-    .catch((error) => {
+    .catch((error) => { //si livre non trouvé
       res.status(400).json({ error });
     });
 };
 
-// suppression d'un livre
+// SUPPRESSION D'UN LIVRE
 exports.deleteBook = (req, res) => {
-  // sélection du livre par son id
-  Book.findOne({ _id: req.params.id })
-    .then((book) => {
-      // on compare l'id de la base de donnée à celle de la requête
-      if (book.userId != req.auth.userId) {
-        res.status(403).json({ message: "Requête non autorisée" });
-      } else {
+
+  Book.findOne({ _id: req.params.id })// recherche du livre correspondant à l'ID fourni dans la requête (req.params.id)
+    .then((book) => {//livre trouvé
+      
+      if (book.userId != req.auth.userId) {// on compare l'id de l'utilisateur du livre à celle de l'utilisateur authentifié
+        res.status(403).json({ message: "Requête non autorisée" }); //si les id sont différentes : interdit
+      
+      } else { // si les 2 id sont identiques
         const filename = book.imageUrl.split("/images/")[1];
-        fs.unlink(`images/${filename}`, () => {
-          Book.deleteOne({ _id: req.params.id })
+        
+        fs.unlink(`images/${filename}`, () => { //suppression du fichier image associé au livre avec fs.unlink
+          Book.deleteOne({ _id: req.params.id }) //suppression du livre de la base de donnée
             .then(() => {
-              res.status(200).json({ message: "Livre supprimé !" });
+              res.status(200).json({ message: "Livre supprimé !" });//succès
             })
-            .catch((error) => res.status(401).json({ error }));
+            .catch((error) => res.status(401).json({ error }));//échec
         });
       }
     })
-    .catch((error) => {
+    .catch((error) => {//livre non trouvé
       res.status(500).json({ error });
     });
 };
 
-// renvoie un seul livre séléctionné grâce à son id
+//RECCUPERATION D'UN LIVRE PAR SON ID
 exports.getOneBook = (req, res) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
+
+  Book.findOne({ _id: req.params.id }) //recherche le livre correspondant à l'id fourni dans la requête (req.params.id)
+
+    .then((book) => res.status(200).json(book)) //livre trouvé, renvoi code 200 avec les détails du livre au format JSON
+    .catch((error) => res.status(404).json({ error })); //livre non trouvé
 };
 
-// renvoie un tableau de tous les livres
+//RECCUPERATION DE TOUS LES LIVRES
 exports.getAllBooks = (req, res) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
+
+  Book.find()//reccupère tous les livres de la base de données
+    .then((books) => res.status(200).json(books))//livres trouvés, renvoi code 200 avec les livres au format JSON
+    .catch((error) => res.status(400).json({ error }));//livre non trouvé
 };
 
+//ATTRIBUE UNE NOTE A UN LIVRE
 exports.ratingBook = (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => {
-      const ratingObject = {
-        userId: req.auth.userId,
-        grade: req.body.rating,
+
+  Book.findOne({ _id: req.params.id }) //recherche le livre correspondant à l'ID fourni dans la requête (req.params.id)
+    .then((book) => {//si livre trouvé
+      const ratingObject = {//crée un objet ratingObject contenant : 
+        userId: req.auth.userId,//l'ID de l'utilisateur
+        grade: req.body.rating,//la note fournie dans la requête (req.body.rating)
       };
 
-      const newRatings = [...book.ratings];
+      const newRatings = [...book.ratings];//crée un nouveau tableau newRatings en copiant les données du tableau ratings
 
-      const hasRated = newRatings.some(
+      const hasRated = newRatings.some(//on vérifie si l'utilisateur a déjà attribué une note à ce livre en parcourant les notes existantes du livre avec some
         (rating) => rating.userId === req.auth.userId
       );
 
       if (req.body.rating >= 0 && req.body.rating <= 5) {
-        if (hasRated) {
+        if (hasRated) {//si l'utilisateur a déjà attribué une note
           return res
             .status(400)
             .json({ message: "Vous avez déjà noté ce livre" });
-        } else {
-          // on ajoute la nouvelle note au tableau ratings
-          newRatings.push(ratingObject);
+        } else {//Si l'utilisateur n'a pas encore attribué une note
+          newRatings.push(ratingObject);//on ajoute la nouvelle note au tableau ratings
           //on ajoute la nouvelle note
-          const totalRatings = newRatings.length;
-          const sumRatings = newRatings.reduce(
-            (acc, rating) => acc + rating.grade,
+          const totalRatings = newRatings.length; //nombre total de note dans le tableau newRatings
+          const sumRatings = newRatings.reduce( //Cela correspond à la somme de toutes les notes dans le tableau newRatings
+            (acc, rating) => acc + rating.grade,// la méthode reduce renvoie la somme totale des notes
             0
           );
-          const newAverageRating = sumRatings / totalRatings;
+          const newAverageRating = sumRatings / totalRatings; //nouvelle note moyenne du livre après l'ajout de la nouvelle note
 
-          Book.updateOne(
-            { _id: req.params.id },
+          Book.updateOne(//met à jour les données du livre dans la base de données avec les nouvelles informations de notation
+            { _id: req.params.id },//spécifie le livre à mettre à jour en fonction de son ID
             {
-              ratings: newRatings,
-              averageRating: newAverageRating,
-              _id: req.params.id,
+              ratings: newRatings,//met à jour le champ ratings du livre avec le tableau newRatings qui contient les nouvelles notes
+              averageRating: newAverageRating,//met à jour le champ averageRating du livre avec la nouvelle note moyenne calculée (newAverageRating)
+              _id: req.params.id,//garantit que l'ID du livre reste inchangé
             }
           )
-            .then(() => {
+            .then(() => {//livre mis à jour
               res.status(200).json(book);
             })
-            .catch((error) => {
+            .catch((error) => {//erreur
               res.status(500).json({ error });
             });
         }
       }
     })
-    .catch((error) => res.status(500).json({ error }));
+
+    .catch((error) => res.status(500).json({ error }));//livre non trouvé
 };
 
-// renvoie un tableau des 3 livres les mieux notés
+//RECCUPERER LES 3 LIVRES LES MIEUX NOTÉS
 exports.bestRating = (req, res, next) => {
-  console.log("e");
-  Book.find()
-    .then((books) => {
-      console.log("c", books);
-      const bestRating = books
-        .sort(
-          (currentIndex, nextIndex) =>
-            nextIndex.averageRating - currentIndex.averageRating
-        )
-        .splice(0, 3);
-      console.log("b", bestRating);
-      res.status(200).json([{ title: "aaaa" }]);
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-};
 
-exports.test = (req, res) => {
-  console.log("test test");
+  Book.find()
+    .then((books) => {//récupère tous les livres de la base de données
+      const bestRatingBook = books
+        .sort(//trie les livres en fonction de leur note moyenne avec sort
+          (currentIndex, nextIndex) => nextIndex.averageRating - currentIndex.averageRating
+        )
+        .splice(0, 3);// renvoie les trois premiers livres
+      res.status(200).json(bestRatingBook);//renvoie les meilleurs livres au format JSON
+    })
+    .catch((error) => {// erreur lors de la reccupération des livres
+      res.status(400).json({ error });
+    });
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,47 +1,54 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt"); //utilisé pour le hachage sécurisé des mots de passe
+const jwt = require("jsonwebtoken"); //utilisé pour la génération et la vérification des token d'authentification
 
-const User = require("../models/user");
+const dotenv = require('dotenv'); //utilisé pour charger les variables d'environnement à partir du fichier .env
+const result = dotenv.config();
 
-exports.signup = (req, res) => {
+const User = require("../models/user"); //modèle utilisateur défini dans le fichier user.js
+
+exports.signup = (req, res) => {// permet à un utilisateur de créer un compte
+
   bcrypt
-    .hash(req.body.password, 10)
+    .hash(req.body.password, 10)//bcrypt.hash utilisé pour hacher le mot de passe fourni par l'utilisateur
     .then((hash) => {
-      const user = new User({
+
+      const user = new User({ //nouvel utilisateur créé en utilisant le modèle User et les informations fournies par l'utilisateur
         email: req.body.email,
         password: hash,
       });
+
       user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(400).json({ error }));
+        .save()// nouvel utilisateur sauvegardé dans la base de données
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))// sauvegarde réussie
+        .catch((error) => res.status(400).json({ error })); //erreur lors de la création de l'utilisateur
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ error }));//erreur lors de la requête
 };
 
-exports.login = (req, res) => {
-  User.findOne({ email: req.body.email })
+exports.login = (req, res) => {//permet à un utilisateur de se connecter avec ses informations d'identification
+  
+  User.findOne({ email: req.body.email })//recherche dans la base de données un utilisateur correspondant à l'adresse e-mail fournie dans la requête
     .then((user) => {
-      if (!user) {
+      if (!user) {//Si aucun utilisateur n'est trouvé, c'est que l'adresse e-mail n'est pas enregistrée dans la base de données
         return res
-          .status(401)
+          .status(401)//non autorisé
           .json({ message: "Paire login/mot de passe incorrecte" });
       }
+      //si un utilisateur correspondant à l'adresse e-mail est trouvé
       bcrypt
-        .compare(req.body.password, user.password)
+        .compare(req.body.password, user.password)//compare le mot de passe fourni dans la requête (req.body.password) avec le mot de passe haché stocké dans la base de données (user.password)
         .then((valid) => {
-          if (!valid) {
+          if (!valid) {//si les mots de passe ne correspondent pas
             return res
-              .status(401)
+              .status(401)//non autorisé
               .json({ message: "Paire login/mot de passe incorrecte" });
           }
-          res.status(200).json({
-            userId: user._id,
-            //chaine de caractère secret aléatoire
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET"),
+          res.status(200).json({//si les mots de passe correspondent c'est que l'utilisateur est authentifié avec succès
+            userId: user._id,//reponse json contenant l'id de l'utilisateur 
+            token: jwt.sign({ userId: user._id }, `${process.env.RANDOM_TOKEN_SECRET}`), //et le token généré par la méthode jwt.sign
           });
         })
-        .catch((error) => res.status(500).json({ error }));
+        .catch((error) => res.status(500).json({ error }));//si erreur dans la comparaison des mots de passe
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ error }));//si erreur dans la recherche de l'utilisateur
 };
