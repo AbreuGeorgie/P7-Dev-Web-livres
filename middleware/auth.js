fe929c27e5bba6c23466a38b44d0f0b9e02a20c5
AbreuGeorgie <abreu.georgie@gmail.com>
2023-06-16 12:31:00 +0200

    ajout de la variable d'environnement

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,19 @@
-const jwt = require ('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+
+const dotenv = require('dotenv');
+const result = dotenv.config();
 
 module.exports = (req, res, next) => {
-    try {
-const token = req.headers.authorization.split(' ')[1];
-const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-const userId = decodedToken.userId;
-req.auth = {
-    userId: userId
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    //chaine de caractère aléatoire meme que l'autre
+    const decodedToken = jwt.verify(token, `${process.env.RANDOM_TOKEN_SECRET}`);
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
+  }
 };
-next()
-    } catch(error) {
-res.status(401).json({ error })
-    }
-}
